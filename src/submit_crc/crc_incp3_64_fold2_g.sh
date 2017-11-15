4d4d1ce65ea5dead146abab27e9e6736d07dfcb0
Rene bidart <rbbidart@gra-login2.graham.sharcnet>
2017-11-15 08:25:16 -0500

    not much

---
+++ src/submit_crc/crc_incp3_64_fold2_g.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:1              # request GPU "generic resource"
+#SBATCH --mem=32000M               # memory per node
+#SBATCH --time=0-18:00            # time (DD-HH:MM)
+#SBATCH --output=crc_incp3_64_f2.out    # %N for node name, %j for jobID
+
+module load cuda cudnn python/3.5.2
+source tensorflow3/bin/activate
+
+python /home/rbbidart/cancer_hist/src/crc_train.py /home/rbbidart/project/rbbidart/cancer_hist/crc_2_fold/crc_64_fold_1 /home/rbbidart/cancer_hist_out/crc/incp3_64_f2 120 16 64 conv_incp3
